b6f5c31da2e672a94df9531bbfbd8e4973da6956
sanjay <sanjayvanshi1995@gmail.com>
2026-07-02 14:04:13 +0530

    create department table and relation with employee table

---
+++ app/Models/Department.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Department extends Model
+{
+    protected $fillable = [
+        'name',
+        'code',
+        'description',
+        'status',
+    ];
+    // create relation 1:M with employees
+    public function employees()
+{
+    return $this->hasMany(Employee::class);
+}
+}
